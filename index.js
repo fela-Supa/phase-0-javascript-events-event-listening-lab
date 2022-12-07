06ea89addc1160ce7e8f5bc03146ffd5edc4f259
fela-Supa <alvinaiterra@gmail.com>
2022-12-07 15:55:51 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input =  document.getElementById("input");
+function alert_me(){
+    alert("I was clicked");
+}
+input.addEventListener("click",alert_me);
 }
